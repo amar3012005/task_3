c24370ab97b390c24ede5855b43481f1f4e02efc
amar3012005 <148769827+amar3012005@users.noreply.github.com>
2024-07-23 12:39:19 +0530

    detect_aruco.cpp

--- src/task_3/src/detect_aruco.cpp
+++ src/task_3/src/detect_aruco.cpp
@@ -29,7 +29,7 @@ private:
         }
         std::vector<int> marker_ids;
         std::vector<std::vector<cv::Point2f>> marker_corners;
-        cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+        cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_1000);
         cv::aruco::detectMarkers(frame, dictionary, marker_corners, marker_ids);
 
         
